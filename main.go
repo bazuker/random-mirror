c624647846a31838ca340b5bf9a0d9888410d0e1
kisulken <daniel.furmanov@gmail.com>
2018-09-05 21:26:33 -0400

    port flag added

--- main.go
+++ main.go
@@ -3,17 +3,22 @@ package main
 import (
 	"flag"
 	"log"
+	"strconv"
 	"truerandom-mirror/host"
 )
 
 func main() {
 	key := ""
+	port := 7890
 	flag.StringVar(&key, "key", "", "unique key address that accepts new numbers from the remote computer")
+	flag.IntVar(&port, "port", 7890, "host port")
 	flag.Parse()
 	if len(key) < 1 {
 		panic("key was not provided")
 	}
-	log.Println("Running the mirror... key is", key)
+	log.Println("Port:", port)
+	log.Println("Key:", key)
+	log.Println("Running the mirror ...")
 	server := host.NewRandomNumberReceiver()
-	server.ListenAndServer(":7890", key)
+	server.ListenAndServer(":"+strconv.Itoa(port), key)
 }
